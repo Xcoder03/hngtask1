87462598fb922bed09b148de04b12cda8e10f0f8
Xcoder <chindaebube03@gmail.comm>
2024-07-01 17:27:03 +0100

    created and finished hello.js

---
+++ controllers/hello.js
@@ -0,0 +1,36 @@
+import ipify from ipify
+import openweathermap  from openweathermap
+
+export class HelloController {
+    async getHello(req, res) {
+      const visitorName = req.query.visitor_name;
+      const ip = req.ip;
+      const location = await ipify.getLocation(ip);
+      const temperature = await openweathermap.getCurrentTemperature(location.city);
+      const response = {
+        client_ip: ip,
+        location: location.city,
+        greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrees Celsius in ${location.city}.`
+      };
+      res.json(response);
+    }
+  }
+
+  export const helloController  = async(req, res) => {
+      try {
+        const visitorName = req.query.visitor_name;
+        const ip = req.ip;
+        const location = await ipify.getLocation(ip);
+        const temperature = await openweathermap.getCurrentTemperature(location.city);
+
+        const response = {
+            client_ip: ip,
+            location: location.city,
+            greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrees Celsius in ${location.city}.`
+          };
+          res.json(response);
+
+      } catch (error) {
+        
+      }
+  }
