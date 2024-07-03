cd2fac99e136c1765d46301f20eae80a1779a9f2
Xcoder <chindaebube03@gmail.comm>
2024-07-03 11:52:14 +0100

    added entry point

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import helloRoute from '../routes/route.js' // Adjust the path as necessary
+import helloRoute from '../routes/helloRoute.js';
 
 const app = express();
 
 app.use(helloRoute);
 
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
